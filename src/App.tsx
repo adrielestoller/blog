e795c40cb510aed50ae7a5545ed9d3162904665c
Adriel <adrielestoller@gmail.com>
2024-02-25 01:41:04 -0300

    feat: add [PostCard]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { InfoBox } from "./components/InfoBox";
+import { PostCard } from "./components/PostCard";
 
 export function App() {
   return (
@@ -7,6 +8,7 @@ export function App() {
       <Header />
       <main className="max-w-3xl mx-auto my-12">
         <InfoBox text="0000 - Mês" />
+        <PostCard title="Título Interessante" date="00/00/0000, 00:00h" tags='tag1  tag2 tag3' />
       </main>
     </>
   )
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,18 @@
+interface PostCardProps {
+  title: string;
+  date: string;
+  tags: string[];
+}
+
+
+export function PostCard({ title, date, tags }: PostCardProps) {
+  return (
+    <div className="p-3 my-5 flex flex-col gap-3 rounded-md bg-zinc-300 shadow transition-transform duration-300 hover:shadow-2xl hover:scale-105">
+      <h3 className="w-fit font-bold text-xl cursor-pointer transition-all duration-150 hover:text-zinc-600">{title}</h3>
+      <div className="mr-2 flex gap-1 justify-between">
+        <p>{date}</p>
+        <p>{tags}</p>
+      </div>
+    </div>
+  );
+}
