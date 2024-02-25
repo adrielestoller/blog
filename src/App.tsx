3a388e28f7855863af3810b86193d7bbdad8a4c9
Adriel <adrielestoller@gmail.com>
2024-02-25 01:38:46 -0300

    feat: add folder components && add [InfoBox]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Header } from "./Header";
+import { Header } from "./components/Header";
+import { InfoBox } from "./components/InfoBox";
 
 export function App() {
   return (
     <>
       <Header />
+      <main className="max-w-3xl mx-auto my-12">
+        <InfoBox text="0000 - Mês" />
+      </main>
     </>
   )
 }
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,11 @@
+interface InfoBoxProps {
+  text: string;
+}
+
+export function InfoBox({ text }: InfoBoxProps) {
+  return (
+    <div className="w-60 p-3 my-5 font-bold text-center text-xl cursor-pointer rounded-lg bg-zinc-600 text-white">
+      <h2>{text}</h2>
+    </div>
+  )
+}
