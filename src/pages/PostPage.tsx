fe1ce650bedef328662dceb1c9388fb441c125ff
adrielestoller <adrielestoller@gmail.com>
2024-02-25 02:45:56 -0300

    feat: add new font Merriweather

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,5 +1,20 @@
+import { Header } from "../components/Header";
+
 export function PostPage() {
   return (
-    <h1>Olá MUndo!</h1>
+    <>
+      <Header />
+      <main className="max-w-3xl mx-auto my-12">
+        <div className="text-center flex flex-col gap-3">
+          <h1 className="text-4xl font-bold">Título Interessante</h1>
+          <p className="text-xl italic">00/00/0000, 00:00h</p>
+        </div>
+        <article className="my-4 text-lg font-serif flex flex-col gap-2">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque quae provident corrupti quaerat voluptatem repudiandae cumque! A enim rerum mollitia sit ipsum earum, expedita eos, cupiditate adipisci neque voluptas.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque quae provident corrupti quaerat voluptatem repudiandae cumque! A enim rerum mollitia sit ipsum earum, expedita eos, cupiditate adipisci neque voluptas.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eaque quae provident corrupti quaerat voluptatem repudiandae cumque! A enim rerum mollitia sit ipsum earum, expedita eos, cupiditate adipisci neque voluptas.</p>
+        </article>
+      </main>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Poppins'
+        sans: 'Poppins',
+        serif: 'Merriweather'
       }
     },
   },
