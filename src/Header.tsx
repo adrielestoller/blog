6c0b803ff280c916185d2268cdd2d0efbff7dbcc
Adriel <adrielestoller@gmail.com>
2024-02-25 01:34:36 -0300

    feat: add [Header]

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <div className="w-full sticky top-0 bg-zinc-800">
+      <div className="flex gap-5 border border-zinc-800">
+        <div className="w-full max-w-xl py-3 text-right bg-white cursor-pointer transition-all duration-300 ease-in-out hover:bg-zinc-800 hover:text-white">
+          <h1 className="mr-24 font-semibold text-4xl">Blog</h1>
+        </div>
+        <div className="w-full bg-zinc-800 box-border">
+        </div>
+      </div>
+    </div>
+  )
+} 
