a85138a7356a83713fc2b73a0d71ce9942e082d9
adrielestoller <adrielestoller@gmail.com>
2024-02-25 06:10:01 -0300

    style: changed background color

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -4,7 +4,7 @@ interface InfoBoxProps {
 
 export function InfoBox({ text }: InfoBoxProps) {
   return (
-    <div className="w-60 p-3 my-5 font-bold text-center text-xl cursor-pointer rounded-lg bg-zinc-600 text-white">
+    <div className="w-60 p-3 my-5 font-bold text-center text-xl cursor-pointer rounded-lg bg-zinc-800 text-white">
       <h2>{text}</h2>
     </div>
   )
